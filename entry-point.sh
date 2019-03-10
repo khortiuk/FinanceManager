4517d8a772c5f308a5a2c597e0ecefb1f7c91e6a
Serhii Khortiuk <khortiukserhii@ukr.net>
2019-03-10 19:57:17 +0200

    added requirements

--- entry-point.sh
+++ entry-point.sh
@@ -11,6 +11,6 @@ tail -n 0 -f ./logs/gunicorn*.log &
 
 gunicorn --bind :8000 personal_finance_manager.wsgi:application --reload
 --log-level=info --log-file=./logs/gunicorn.log --access-logfile=
-./logs/gunicorn-access.log --name=finance-manager
+./logs/gunicorn-access.log
 
 exec "$@"
